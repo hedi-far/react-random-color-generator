84c50caa98d5f95643c557e25af6ae211926f0b7
Hedwig Farthofer <hedwig.farthofer@outlook.at>
2020-09-15 16:16:36 +0200

    Deleted Colors.js and ColoredBox.js and ColoredBox.css

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-import React, { useState, useRef } from 'react';
-import ColoredBox from './ColoredBox';
+import React, { useState } from 'react';
 import './App.css';
+const randomColor = require('randomcolor');
 
 function App() {
-  const [color, setColor] = useState([]);
-  //const inputColorRef = useRef();
+  const [backgroundColor, setColor] = useState('#FFFFFF');
+
+  const randomizedColor = randomColor();
 
   function handleChangeColor(e) {
-    //const hex = inputColorRef.current.value;
-    setColor([]);
+    setColor(randomizedColor);
   }
 
   return (
     <div>
       <h1>React Random Color Generator</h1>
-      <ColoredBox />
-      {/*<input ref={inputColorRef} type="text"></input> */}
+      <div
+        id="box"
+        style={{
+          textSlign: 'center',
+          boxSizing: 'border-box',
+          borderStyle: 'dotted',
+          height: '200px',
+          backgroundColor: backgroundColor,
+        }}
+      >
+        <h1>{backgroundColor}</h1>
+      </div>
       <button onClick={handleChangeColor}>Change color!</button>
     </div>
   );
--- src/Color.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-var randomColor = require('randomcolor');
-
-export default function Color() {
-  return randomColor();
-}
